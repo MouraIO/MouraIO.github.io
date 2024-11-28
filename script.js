ec8cdbb027105bbd6eb9d933710333141b983f8f
MouraSS <murilop387@gmail.com>
2024-11-28 19:15:56 -0300

    restauracao

--- script.js
+++ script.js
@@ -26,21 +26,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Pegar todas as imagens do banner
   const bannerImages = document.querySelectorAll(".banner img");
-  
-  // Ativar a primeira imagem quando a página carregar
+
   bannerImages[0].classList.add("active");
-  
-  // Adicionar o evento de clique em cada imagem
+
   bannerImages.forEach((img) => {
     img.addEventListener("click", () => {
-      // Remover active de todas as imagens
       bannerImages.forEach((image) => {
         image.classList.remove("active");
       });
-      
-      // Adicionar active na imagem clicada
+
       img.classList.add("active");
     });
   });
