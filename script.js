e7688f1ab492ad1ecfd6e4896656aeb36aa99460
MouraSS <murilop387@gmail.com>
2024-11-28 15:05:56 -0300

    correcao

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const gamesLink = document.querySelector(".has-submenu");
+  const gamesLink = document.querySelector("#games-menu");
   const gamesGridMenu = document.querySelector(".games-grid-menu");
+  const sportsLink = document.querySelector("#sports-menu");
+  const sportsGridMenu = document.querySelector(".sports-grid-menu");
 
-  gamesLink.addEventListener("click", function (e) {
-    e.preventDefault();
-    this.classList.toggle("active");
-    gamesGridMenu.classList.toggle("active");
-  });
+  function toggleSubmenu(link, menu) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      this.classList.toggle("active");
+      menu.classList.toggle("active");
+    });
+  }
+
+  toggleSubmenu(gamesLink, gamesGridMenu);
+  toggleSubmenu(sportsLink, sportsGridMenu);
 
   document.addEventListener("click", function (e) {
     if (!gamesLink.contains(e.target) && !gamesGridMenu.contains(e.target)) {
       gamesLink.classList.remove("active");
       gamesGridMenu.classList.remove("active");
+    }
+    if (!sportsLink.contains(e.target) && !sportsGridMenu.contains(e.target)) {
+      sportsLink.classList.remove("active");
+      sportsGridMenu.classList.remove("active");
     }
   });
 
